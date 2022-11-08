d68d42fe2b201e26bd375d75a5c03d24e561d457
brainy <khoshfekr2001@protonmail.com>
2022-11-08 17:34:04 +0330

    hashing password - level3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 mongoose.connect(process.env.MONGODB_URL);
 
@@ -11,9 +11,6 @@ const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
-
-
-userSchema.plugin(encrypt, { secret: process.env.SECRETS, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -56,7 +53,7 @@ app.route("/register")
 .post(function (req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function (err) {
